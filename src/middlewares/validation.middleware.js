5045af838bae4862198e10718d299e0697566e0a
Lucas Ubinge <ubinge27@gmail.com>
2025-08-10 17:13:50 -0300

    validate - receiver

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -2,13 +2,11 @@ export const validate = (schema) => (req, res, next) => {
   try {
     schema.parse({
       body: req.body,
-      query: req.query,
-      params: req.params,
     });
     next();
   } catch (error) {
     return res.status(400).json({
-      error: "Dados inválidos.",
+      error: "válidos.",
       // Usar o método flatten() do Zod para uma resposta de erro mais estruturada
       details: error.flatten(),
     });
