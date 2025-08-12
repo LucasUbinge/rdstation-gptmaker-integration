91eaab054fb70de0fc79185d8d69e3397f6e7743
Lucas Ubinge <ubinge27@gmail.com>
2025-08-11 21:06:52 -0300

    Atualização da mensagem

--- src/config/templates.js
+++ src/config/templates.js
@@ -1,6 +1,12 @@
 export const templates = {
   welcome: (name) =>
-    `Olá, ${name}! Bem-vindo à nossa plataforma. Estamos felizes em ter você conosco.`,
+    `Olá, ${name}! Obrigado por se conectar com a SV7 Company. 
+
+Somos especialistas em prospecção e qualificação de leads B2B em todo o Brasil, conectando empresas a clientes ideais com o apoio de tecnologia, dados e estratégia.
+
+💡 Da uma olhada: www.sv7.com.br
+
+Responda esta mensagem e vamos conversar sobre as melhores oportunidades para acelerar as vendas B2B da sua empresa.`,
   // ... no futuro, você pode adicionar outros templates aqui
   // ex: statusUpdate: (name, status) => `Olá, ${name}! Seu pedido agora está com o status: ${status}.`
 };
